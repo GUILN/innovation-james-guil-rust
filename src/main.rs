36a7b7e024a6a333696630ceec8b5118ff1b0971
Guilherme Leonardo Nunes <guilherme.l.nunes@gmail.com>
2021-10-05 15:11:14 +0200

    added logger

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, /*post, web,*/ middleware, App, HttpResponse, HttpServer, Responder};
+use std::{env, io};
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+        env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+        env_logger::init();
+
         HttpServer::new(|| {
                 App::new()
-                    .service(hello)})
+                    .wrap(middleware::Logger::default())
+                    .service(hello)
+            })
             .bind("127.0.0.1:8080")?
-                    .run()
-                    .await
+            .run()
+            .await
+        
 }
-
-
 
 #[get("/")]
 async fn hello() -> impl Responder { 
     HttpResponse::Ok().body("hello !")
 } 
+
+
 
